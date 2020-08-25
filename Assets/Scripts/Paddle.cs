5a628ebb80f7f5ace80d64d616f2b0c301c6a825
Son Nguyen <sonnguyen9800@gmail.com>
2020-08-26 02:53:21 +0700

    Two mode to move the paddle, physic and velocity

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     private float speed;
-    Vector3 movement;
+    Vector2 movement;
     private MoveComponent moveComponent;
+    public Rigidbody2D rb;
+
+    private bool flag_controlled_force = false;
+
     private void Awake()
     {
         moveComponent = GetComponent<MoveComponent>();
@@ -14,35 +18,31 @@ public class Paddle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        movePadde(movement);
+    }
+    void movePadde(Vector2 direction)
+    {
+        if (this.flag_controlled_force == true)
+        {
+            rb.AddForce(direction * this.speed, ForceMode2D.Force);
+        } else
+        {
+            rb.velocity = direction * this.speed;
+        }
+   
         
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            Debug.Log(" Wall coming ");
-            Rigidbody rbdy = collision.gameObject.GetComponent<Rigidbody>();
-
-            //Stop Moving/Translating
-            rbdy.velocity = Vector3.zero;
-
-            //Stop rotating
-            rbdy.angularVelocity = Vector3.zero;
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
         speed = moveComponent.getSpeed();
-
-        movement = new Vector3(moveHorizontal, 0f, 0f);
-        movement = movement * speed * Time.deltaTime;
-
-       
-            transform.position += movement;
-        
-        
+        movement = new Vector2(moveHorizontal, moveVertical);
     }
 }
