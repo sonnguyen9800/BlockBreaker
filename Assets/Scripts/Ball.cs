ee8a9a462f6643454203474be9631ed02f461d0c
Khai Truong <kd.kidr@gmail.com>
2020-08-26 17:22:03 +0700

    Finish game logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,43 +1,46 @@
-﻿using System.Collections;
+﻿#pragma warning disable 0649
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public bool isPlay; // game state: if the ball is playing
-    public Transform paddle; //get the paddle position
-    public float speed;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform paddle; //get the paddle position
+    [SerializeField]
+    private float force = 10f;
+    private Rigidbody2D rb;
+    private bool isOnPaddle = true;
+    public bool HasTouchBottom { get; set; }
+    private void Awake()
     {
-        // Get the body of the component which add in the object
         rb = GetComponent<Rigidbody2D>();
-
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        if (!isPlay)
+        MoveToPaddle();
+    }
+    private void Update()
+    {
+        if (isOnPaddle)
         {
-            transform.position = paddle.position; // the ball position in the paddle
-        }
-
-        if (Input.GetButtonDown("Jump") && !isPlay) // press the Spacebar and release the ball
-        {
-            isPlay = true;
-            rb.AddForce(Vector2.up * speed);
-
+            transform.position = paddle.position;
         }
     }
-    void OnTriggerEnter2D(Collider2D collision)
+    public void LaunchBall()
     {
-        if (collision.CompareTag("bottom")) // check if the ball hit that object
-        {
-            Debug.Log("You lose!!");
-            rb.velocity = Vector2.zero; // kill all the force if the ball hit the bottom
-            isPlay = false;
-        }
+        isOnPaddle = false;
+        rb.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+    }
+    public void MoveToPaddle()
+    {
+        HasTouchBottom = false;
+        isOnPaddle = true;
+        rb.velocity = Vector2.zero;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
