c3dccf87ba0c92c00e3153746e1e55de35cefc34
Trung-RMIT-3695656 <s3695656@rmit.edu.vn>
2020-08-26 10:23:28 +0700

    Script for Scene Changer

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+   public void startGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
